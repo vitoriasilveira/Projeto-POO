18e6b24c7e1787e8f4bc48637e8e46a610a140c6
Vitória Silveira <vitoria.silveira@unesp.br>
2024-06-22 18:21:37 -0300

    remoção de validação na Classe Mensagem

--- AppRU/src/main/java/br/unesp/rc/appru/modelo/Mensagem.java
+++ AppRU/src/main/java/br/unesp/rc/appru/modelo/Mensagem.java
@@ -15,41 +15,19 @@ public class Mensagem implements Runnable  {
         this.venda = venda;
     }
     
-    private void enviarSms(){
+
+    @Override
+    public void run() {
         System.out.println("FROM: noreplay@unesprc.br \nTO: " + venda.getComprador().getNome());
         System.out.println(" Olá, " + venda.getComprador().getNome() 
                 + ". A compra do ticket do Restaurante Universitário de Rio Claro poderá ser realizada. Entre em contato com o vendedor(a) " + venda.getVendedor().getNome()
                 + " para efetuar sua compra.");
         System.out.println("---------------------");
         System.out.println("DADOS de COMPRA e VENDA");
-        System.out.println("    Código: " +venda.getIdVenda());
         System.out.println("    Vendedor(a): "+venda.getVendedor().getNome());
         System.out.println("    Telefone: "+venda.getVendedor().getTelefone());
-        System.out.println("---------------------");
-    }   
-    
-    
-    private void enviarEmail(){
-        System.out.println("FROM: noreplay@unesprc.br \nTO: " + venda.getComprador().getNome());
-        System.out.println(" Olá, " + venda.getComprador().getNome() 
-                + "a compra do ticket do Restaurante Universitário de Rio Claro poderá ser realizada. Entre em contato com o vendedor(a) " + venda.getVendedor().getNome()
-                + "para efetuar sua compra.");
-        System.out.println("---------------------");
-        System.out.println("DADOS de COMPRA e VENDA");
-        System.out.println("    Código: " +venda.getIdVenda());
-        System.out.println("    Vendedor(a): "+venda.getVendedor().getNome());
         System.out.println("    Email: "+venda.getVendedor().getEmail());
-        System.out.println("---------------------");
-    }
-
-    @Override
-    public void run() {
-        if(venda.getComprador().getTipo()== 0){
-            enviarSms();
-        }
-        else if(venda.getComprador().getTipo()== 1){
-            enviarEmail();
-        }
+        System.out.println("---------------------");  
     }
 }
 
